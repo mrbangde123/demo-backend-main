03a332cafb65ee6ef64a0c01f577b22585edd6b9
mrbangde123 <860217746@qq.com>
2026-05-10 14:03:56 +0800

    feat: init spring boot skeleton via spring initializr

---
+++ src/main/java/com/hehui/main/DemoBackendMainApplication.java
@@ -0,0 +1,13 @@
+package com.hehui.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehui/main/DemoBackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehui.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
